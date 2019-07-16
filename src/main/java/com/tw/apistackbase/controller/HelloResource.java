ae92466a3369398720c95b1aea233f9e6f95d4b0
scauzml <13828607279@163.com>
2019-07-16 23:30:25 +0800

    提交

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,27 +1,61 @@
-package com.tw.apistackbase.controller;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.logging.Logger;
-
-/**
- * Created by jxzhong on 18/08/2017.
- */
-@RestController
-@RequestMapping("/hello")
-public class HelloResource {
-
-    private final Logger log = Logger.getLogger(this.getClass().getName());
-
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
-
-        return ResponseEntity.ok("Hello:" + userName);
-    }
-
-
-}
+//package com.tw.apistackbase.controller;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.logging.Logger;
+//import java.util.stream.Collectors;
+//
+///**
+// * Created by jxzhong on 18/08/2017.
+// */
+//@RestController
+//@RequestMapping("/employees")
+//public class HelloResource {
+//
+//    private final Logger log = Logger.getLogger(this.getClass().getName());
+//    Map<Integer, Employee> map = new HashMap<>();
+//
+//
+////    @GetMapping(path = "/{userName}", produces = {"application/json"})
+////    public ResponseEntity<String> getAll(@PathVariable String userName) {
+////
+////        return ResponseEntity.ok("Hello:" + userName);
+////    }
+//
+//    @GetMapping()
+//    public ResponseEntity getAll() {
+//
+//
+//        return ResponseEntity.ok().body(map);
+//    }
+//
+//    @PostMapping()
+//    public ResponseEntity create(@RequestBody Employee employee) {
+//
+//        map.put(employee.getId(), employee);
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+//    }
+//
+//    @PutMapping("/{userid}")
+//    public ResponseEntity change(@RequestBody Employee employee,@PathVariable("userid")String id) {
+//        System.out.println(employee.toString());
+//        Employee employee1 = map.get(Integer.valueOf(id));
+//        employee1=employee;
+//        map.put(employee1.getId(), employee1);
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+//
+//    }
+//
+//    @DeleteMapping("/{userid}")
+//    public ResponseEntity delete(@PathVariable("userid") String id) {
+//        map.remove(Integer.valueOf(id));
+//        return ResponseEntity.ok().build();
+//    }
+//}
--- src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
+++ src/test/java/com/tw/apistackbase/ApiStackBaseApplicationTests.java
@@ -1,17 +1,38 @@
 package com.tw.apistackbase;
 
+import com.tw.apistackbase.controller.Employee;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-@ExtendWith(SpringExtension.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApiStackBaseApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void postTest()throws Exception {
+		Employee employee = new Employee(1, "dd", 33, "man");
+		JSONObject jsonObject = new JSONObject(employee);
+		jsonObject.toString();
+		this.mockMvc.perform(post("/employees").contentType(MediaType.APPLICATION_JSON_UTF8).content(jsonObject.toString())).andExpect(status().isCreated());
+
+
+	}
 }
